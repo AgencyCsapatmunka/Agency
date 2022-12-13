a4e5b6c43ab666db711f22cfcfdd0e51a2f39462
kovacsdaniel422 <kovacsdaniel422@gmail.com>
2022-12-13 10:08:59 +0100

    users
    Users tábla adatokkal

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -17,12 +18,17 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->boolean('vip')->default(0);
             $table->timestamps();
         });
+        User::create(['name' => 'Gréta Fekete' , 'email'=> 'G.Fekete@mail.com','vip'=>0]);
+        User::create(['name' => 'János Pap' , 'email'=> 'J.Pap@mail.com','vip'=>0]);
+        User::create(['name' => 'Ádám Fekete' , 'email'=> 'A.Fekete@mail.com','vip'=>0]);
+        User::create(['name' => 'Botond Székely' , 'email'=> 'Botond.Sz@mail.com','vip'=>1]);
+        User::create(['name' => 'Bendegúz Kerekes' , 'email'=> 'K.Bendeguz@mail.com','vip'=>0]);
+        User::create(['name' => 'Imre Hegedűs' , 'email'=> 'I.Hegedus@mail.com','vip'=>1]);
     }
+
 
     /**
      * Reverse the migrations.
